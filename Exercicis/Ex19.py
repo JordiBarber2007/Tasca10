f05c223dad7924324a5589d0156000c97054cff5
JordiBarber2007 <jbarber20716@iesjoanramis.org>
2023-12-14 09:28:56 +0100

    He modificat el 14 i el 16

--- Exercicis/Ex19.py
+++ Exercicis/Ex19.py
@@ -1,4 +1,6 @@
 def es_palindrom(paraula):
+    # Prec: li hem de passar una paraula/string
+    # Post: Retorna True si és un palindrom i False si no ho és
     #Convertim tots els carácters en minúscules per considerar la majúscula y minúscula equivalents.
     paraula = paraula.lower()
     return paraula == paraula[::-1]
